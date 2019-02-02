378edb3fbae89c9665b3edce9d07d342cedfad4e
superbeom <it.super.beom@gmail.com>
2019-02-02 21:21:29 +0900

    Updates

---
+++ precache-manifest.ce622a47b89fb9a839327f019e7e3e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "96d7c51a0426d45bef6e",
+    "url": "/movie_app/static/js/main.96d7c51a.chunk.js"
+  },
+  {
+    "revision": "be9ff9f0d6b66aea30bf",
+    "url": "/movie_app/static/js/1.be9ff9f0.chunk.js"
+  },
+  {
+    "revision": "96d7c51a0426d45bef6e",
+    "url": "/movie_app/static/css/main.2b447477.chunk.css"
+  },
+  {
+    "revision": "607dc6a8d2c5c178bd69520d18bd409e",
+    "url": "/movie_app/index.html"
+  }
+];
