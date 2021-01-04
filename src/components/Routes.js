64c8bbd1784705fd8d7af3ce490665325e266e15
superbeom <it.super.beom@gmail.com>
2021-01-04 12:29:24 +0900

    Origin Code - Remove withRouter

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,33 +1,23 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Main from "../Routes/Main";
 import TodoSecretary from "../Routes/TodoSecretary";
 import GuessMyNumber from "../Routes/GuessMyNumber";
 import FlipCard from "../Routes/FlipCard";
+import AppAds from "../Routes/AppAds";
 import HeungdoLabPolicy from "../Routes/HeungdoLabPolicy";
+import NotFound from "../Routes/NotFound";
 
-export default withRouter(({ history }) => {
-  const {
-    location: { pathname },
-  } = history;
-
-  switch (true) {
-    case pathname === "/":
-      return <Main />;
-
-    case pathname === "/todosecretary":
-      return <TodoSecretary />;
-
-    case pathname === "/guessmynumber":
-      return <GuessMyNumber />;
-
-    case pathname === "/flipcard":
-      return <FlipCard />;
-
-    case pathname === "/policy/heungdolab":
-      return <HeungdoLabPolicy />;
-
-    default:
-      return <Main />;
-  }
-});
+export default () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route exact path="/todosecretary" component={TodoSecretary} />
+      <Route exact path="/guessmynumber" component={GuessMyNumber} />
+      <Route exact path="/flipcard" component={FlipCard} />
+      <Route exact path="/app-ads.txt" component={AppAds} />
+      <Route exact path="/policy/heungdolab" component={HeungdoLabPolicy} />
+      <Route path="*" component={NotFound} />
+    </Switch>
+  );
+};
