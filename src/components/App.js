496459b8c963d88c47b433fe3430192cbf816db3
superbeom <it.super.beom@gmail.com>
2021-01-04 17:33:46 +0900

    Change Router - BrowserRouter to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../styles/GlobalStyles";
 import Theme from "../styles/Theme";
